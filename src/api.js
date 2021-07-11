a497b0d894d76ea492390d592c372626692e1e8c
M-Rb3 <rabea9220@gmail.com>
2021-07-11 02:28:35 +0200

    popular games url

---
+++ src/api.js
@@ -0,0 +1,32 @@
+const base_url = "https://api.rawg.io/api/";
+
+const getMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return "0" + month;
+  } else {
+    return month;
+  }
+};
+const getDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return "0" + day;
+  } else {
+    return day;
+  }
+};
+
+const currnetYear = new Date().getFullYear();
+const currentMonth = getMonth();
+const currentDay = getDay();
+const currentDate = `${currnetYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currnetYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currnetYear + 1}-${currentMonth}-${currentDay}`;
+
+const popularGames = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesUrl = () => {
+  return base_url + popularGames;
+};
+console.log(popularGamesUrl());
